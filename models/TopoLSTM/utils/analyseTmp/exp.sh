fa5bfb9f1d8768ae508a46f0c9f84f472633836a
hyh_box <yuhang.he.scut@outlook.com>
2022-03-04 14:28:36 +0800

    Delete models/TopoLSTM/utils/analyseTmp directory

--- models/TopoLSTM/utils/analyseTmp/exp.sh
+++
@@ -1,3 +0,0 @@
-python -u gen_cas.py --rawdataset=dataset_dblp.txt --dataset=dblp1/ --observation_time=6 --least_num=10 --up_num=1000
-python -u gen_run.py --rawdataset=dataset_dblp.txt --dataset=dblp1/ --observation_time=6 --least_num=10 --up_num=1000
-python -u run.py --rawdataset=dataset_dblp.txt --dataset=dblp1/ --observation_time=6 --least_num=10 --up_num=1000 > 6.log 
